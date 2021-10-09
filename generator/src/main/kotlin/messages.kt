62e430553974f00a3d2ee7a74d0105cdbfe32e3e
Rafał Borcz <RafalBO99@outlook.com>
2021-10-09 21:23:12 +0200

    Update messages.kt

--- generator/src/main/kotlin/messages.kt
+++ generator/src/main/kotlin/messages.kt
@@ -6,10 +6,10 @@ import io.github.wulkanowy.messages.pojo.MessageType
 val messages = listOf(
     Message(
         id = 2,
-        title = "Problemy z zadaniami domowymi",
+        title = "Problemy z Fakelogiem",
         content = "Przez błąd w systemie UONET+ (potwierdzony przez firmę VULCAN) u niektórych zadania pojawiają się dopiero w dniu terminu ich oddania. W tym czasie prosimy o uważne sprawdzanie zadań w różnych źródłach i prosimy o cierpliwość!",
         targetRegisterHost = "fakelog.cf",
-        priority = MessagePriority.HIGH,
+        priority = MessagePriority.MEDIUM,
         type = MessageType.DASHBOARD_MESSAGE,
     ),
     Message(
