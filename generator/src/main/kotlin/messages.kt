2f3edbde0f30f00d112deb2c627cd96e193f5b8e
Mikołaj Pich <m.pich@outlook.com>
2021-10-13 23:22:38 +0200

    Add some debug messages

--- generator/src/main/kotlin/messages.kt
+++ generator/src/main/kotlin/messages.kt
@@ -5,6 +5,22 @@ import io.github.wulkanowy.messages.pojo.MessageType
 
 val messages = listOf(
     Message(
+        id = 4,
+        title = "Problemy z VULCANem",
+        content = "Logowanie do dziennika przestało działać, dlatego musimy zakończyć działalność.",
+        priority = MessagePriority.LOW,
+        targetFlavor = AppFlavor.PLAY,
+        type = MessageType.DASHBOARD_MESSAGE,
+    ),
+    Message(
+        id = 3,
+        title = "Problemy z VULCANem",
+        content = "Przez błąd w systemie UONET+ (potwierdzony przez firmę VULCAN) u niektórych zadania pojawiają się dopiero w dniu terminu ich oddania. W tym czasie prosimy o uważne sprawdzanie zadań w różnych źródłach i prosimy o cierpliwość!",
+        targetRegisterHost = "fakelog.cf",
+        priority = MessagePriority.HIGH,
+        type = MessageType.DASHBOARD_MESSAGE,
+    ),
+    Message(
         id = 2,
         title = "Problemy z Fakelogiem",
         content = "Przez błąd w systemie UONET+ (potwierdzony przez firmę VULCAN) u niektórych zadania pojawiają się dopiero w dniu terminu ich oddania. W tym czasie prosimy o uważne sprawdzanie zadań w różnych źródłach i prosimy o cierpliwość!",
