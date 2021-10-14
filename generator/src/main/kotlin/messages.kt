7824465f01e3f5706a6b1dab4c61d4de241f9bfd
Rafał Borcz <RafalBO99@outlook.com>
2021-10-14 10:22:24 +0200

    Add message for fakelog

--- generator/src/main/kotlin/messages.kt
+++ generator/src/main/kotlin/messages.kt
@@ -1,3 +1,14 @@
 import io.github.wulkanowy.messages.pojo.Message
+import io.github.wulkanowy.MessagePriority
+import io.github.wulkanowy.MessageType
 
-val messages = listOf<Message>()
+val messages = listOf<Message>(
+    Message(
+        id = 1,
+        title = "Problemy z Fakelogiem",
+        content = "Przez błąd w systemie UONET+ (potwierdzony przez firmę VULCAN) u niektórych zadania pojawiają się dopiero w dniu terminu ich oddania. W tym czasie prosimy o uważne sprawdzanie zadań w różnych źródłach i prosimy o cierpliwość!",
+        targetRegisterHost = "fakelog.cf",
+        priority = MessagePriority.MEDIUM,
+        type = MessageType.DASHBOARD_MESSAGE,
+    )
+)
