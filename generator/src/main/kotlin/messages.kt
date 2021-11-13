c07a16c618742645a10c298bf9d3addbee188cac
Mikołaj Pich <m.pich@outlook.com>
2021-11-13 11:01:38 +0100

    Clear messages

--- generator/src/main/kotlin/messages.kt
+++ generator/src/main/kotlin/messages.kt
@@ -3,12 +3,12 @@ import io.github.wulkanowy.messages.pojo.MessagePriority
 import io.github.wulkanowy.messages.pojo.MessageType
 
 val messages = listOf(
-    Message(
-        id = 1,
-        title = "Problemy z Fakelogiem",
-        content = "Przez błąd w systemie UONET+ (potwierdzony przez firmę VULCAN) u niektórych zadania pojawiają się dopiero w dniu terminu ich oddania. W tym czasie prosimy o uważne sprawdzanie zadań w różnych źródłach i prosimy o cierpliwość!",
-        targetRegisterHost = "fakelog.cf",
-        priority = MessagePriority.MEDIUM,
-        type = MessageType.DASHBOARD_MESSAGE,
-    )
+    //Message(
+    //    id = 1,
+    //    title = "Problemy z Fakelogiem",
+    //    content = "Przez błąd w systemie UONET+ (potwierdzony przez firmę VULCAN) u niektórych zadania pojawiają się dopiero w dniu terminu ich oddania. W tym czasie prosimy o uważne sprawdzanie zadań w różnych źródłach i prosimy o cierpliwość!",
+    //    targetRegisterHost = "fakelog.cf",
+    //    priority = MessagePriority.MEDIUM,
+    //    type = MessageType.DASHBOARD_MESSAGE,
+    //)
 )
