ffb6ab72cd3c35132cf860ccc44e4277bc221f0d
Rafał Borcz <rafal.borcz@goodylabs.com>
2021-11-17 12:20:40 +0100

    Add policy privacy message

--- generator/src/main/kotlin/messages.kt
+++ generator/src/main/kotlin/messages.kt
@@ -2,13 +2,13 @@ import io.github.wulkanowy.messages.pojo.Message
 import io.github.wulkanowy.messages.pojo.MessagePriority
 import io.github.wulkanowy.messages.pojo.MessageType
 
-val messages = listOf<Message>(
-    //Message(
-    //    id = 1,
-    //    title = "Problemy z Fakelogiem",
-    //    content = "Przez błąd w systemie UONET+ (potwierdzony przez firmę VULCAN) u niektórych zadania pojawiają się dopiero w dniu terminu ich oddania. W tym czasie prosimy o uważne sprawdzanie zadań w różnych źródłach i prosimy o cierpliwość!",
-    //    targetRegisterHost = "fakelog.cf",
-    //    priority = MessagePriority.MEDIUM,
-    //    type = MessageType.DASHBOARD_MESSAGE,
-    //)
+val messages = listOf(
+    Message(
+        id = 1,
+        title = "Polityka prywatności",
+        content = "Zaktualizowaliśmy politykę prywatności. Kliknij aby zobaczyć więcej",
+        priority = MessagePriority.LOW,
+        type = MessageType.DASHBOARD_MESSAGE,
+        destinationUrl = "https://wulkanowy.github.io/polityka-prywatnosci"
+    )
 )
