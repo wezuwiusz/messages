75eb2d8320a0934ad1da4a9fffc2ff4ab80fd2ad
Rafał Borcz <RafalBO99@outlook.com>
2021-12-02 13:39:54 +0100

    New message (id 2)

--- generator/src/main/kotlin/messages.kt
+++ generator/src/main/kotlin/messages.kt
@@ -4,12 +4,10 @@ import io.github.wulkanowy.messages.pojo.MessageType
 
 val messages = listOf(
     Message(
-        id = 1,
-        title = "Polityka prywatności",
-        content = "Zaktualizowaliśmy politykę prywatności. Kliknij aby zobaczyć więcej",
-        priority = MessagePriority.LOW,
-        type = MessageType.DASHBOARD_MESSAGE,
-        destinationUrl = "https://wulkanowy.github.io/polityka-prywatnosci",
-        isDismissible = true
+        id = 2,
+        title = "Problemy z aplikacją",
+        content = "Występują problemy z dostępem do systemu UONET+, które są spowodowane ostatnimi aktualizacjami po stronie VULCANa. Nie jest to winą Wulkanowego. Prosimy o cierpliwość do czasu aż VULCAN rozwiąże problem po swojej stronie",
+        priority = MessagePriority.MEDIUM,
+        type = MessageType.DASHBOARD_MESSAGE
     )
 )
