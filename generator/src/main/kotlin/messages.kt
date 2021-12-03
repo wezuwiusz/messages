b1d40c741ba8fa5d6fa78bd4f62da23b09e49432
Rafał Borcz <RafalBO99@outlook.com>
2021-12-03 12:03:40 +0100

    Remove message

--- generator/src/main/kotlin/messages.kt
+++ generator/src/main/kotlin/messages.kt
@@ -1,13 +1,3 @@
 import io.github.wulkanowy.messages.pojo.Message
-import io.github.wulkanowy.messages.pojo.MessagePriority
-import io.github.wulkanowy.messages.pojo.MessageType
 
-val messages = listOf(
-    Message(
-        id = 2,
-        title = "Problemy z aplikacją",
-        content = "Występują problemy z dostępem do systemu UONET+, które są spowodowane ostatnimi aktualizacjami po stronie VULCANa. Nie jest to winą Wulkanowego. Prosimy o cierpliwość do czasu aż VULCAN rozwiąże problem po swojej stronie",
-        priority = MessagePriority.MEDIUM,
-        type = MessageType.DASHBOARD_MESSAGE
-    )
-)
+val messages: List<Message> = listOf()
