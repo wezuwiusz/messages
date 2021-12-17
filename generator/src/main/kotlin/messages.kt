4c9c6544a35ece21253370b8727430ae8b004c87
Mikołaj Pich <m.pich@outlook.com>
2021-12-17 21:15:49 +0100

    Add message for edu.gdansk.pl after register update

--- generator/src/main/kotlin/messages.kt
+++ generator/src/main/kotlin/messages.kt
@@ -4,6 +4,16 @@ import io.github.wulkanowy.messages.pojo.MessageType
 
 val messages = listOf(
     Message(
+        id = 3,
+        title = "Problemy z logowaniem",
+        content = "Przez aktualizację dziennika Gdańskiej Platformy Edukacyjnej przestało działać logowanie w Wulkanowym. Przez to przy odświeżaniu danych pokazuje się komunikat o braku połączenia z internetem. Pracujemy nad poprawką, która powinna pojawić się w aktualizacji do końca tego tygodnia",
+        priority = MessagePriority.HIGH,
+        destinationUrl = "https://twitter.com/wulkanowy/status/1471930756087914511",
+        targetRegisterHost = "edu.gdansk.pl",
+        type = MessageType.DASHBOARD_MESSAGE,
+        isVisible = true,
+    ),
+    Message(
         id = 2,
         title = "Problemy z aplikacją",
         content = "Występują problemy z dostępem do systemu UONET+, które są spowodowane ostatnimi aktualizacjami po stronie VULCANa. Nie jest to winą Wulkanowego. Prosimy o cierpliwość do czasu aż VULCAN rozwiąże problem po swojej stronie",
