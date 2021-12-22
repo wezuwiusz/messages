f7b9d834186ac76ee79e140188349e1ab188a060
Rafał Borcz <RafalBO99@outlook.com>
2021-12-22 09:35:11 +0100

    Update Gdansk messages

--- generator/src/main/kotlin/messages.kt
+++ generator/src/main/kotlin/messages.kt
@@ -1,17 +1,27 @@
-import io.github.wulkanowy.messages.pojo.Message
-import io.github.wulkanowy.messages.pojo.MessagePriority
-import io.github.wulkanowy.messages.pojo.MessageType
-import io.github.wulkanowy.messages.pojo.RegisterAddress
+import io.github.wulkanowy.messages.pojo.*
 
 val messages = listOf(
     Message(
-        id = 3,
+        id = 4,
         title = "Problemy z logowaniem",
-        content = "Przez aktualizację dziennika Gdańskiej Platformy Edukacyjnej przestało działać logowanie w Wulkanowym. Przez to przy odświeżaniu danych pokazuje się komunikat o nieznanym błędzie dziennika. Pracujemy nad poprawką, która powinna pojawić się niedługo",
+        content = "Przez aktualizację dziennika Gdańskiej Platformy Edukacyjnej przestało działać logowanie w Wulkanowym. Przez to przy odświeżaniu danych pokazuje się komunikat o nieznanym błędzie dziennika. W sklepie AppGallery dostępna jest aktualizacja",
         priority = MessagePriority.HIGH,
-        destinationUrl = "https://twitter.com/wulkanowy/status/1471930756087914511",
+        destinationUrl = "https://appgallery.huawei.com/app/C101440411",
         targetRegisterHost = RegisterAddress.EDU_GDANSK.url,
         type = MessageType.DASHBOARD_MESSAGE,
+        targetFlavor = AppFlavor.HMS,
+        versionMax = 101,
+        isVisible = true,
+    ),
+    Message(
+        id = 3,
+        title = "Problemy z logowaniem",
+        content = "Przez aktualizację dziennika Gdańskiej Platformy Edukacyjnej przestało działać logowanie w Wulkanowym. Przez to przy odświeżaniu danych pokazuje się komunikat o nieznanym błędzie dziennika. W sklepie Google Play dostępna jest aktualizacja",
+        priority = MessagePriority.HIGH,
+        destinationUrl = "https://play.google.com/store/apps/details?id=io.github.wulkanowy",
+        targetRegisterHost = RegisterAddress.EDU_GDANSK.url,
+        type = MessageType.DASHBOARD_MESSAGE,
+        targetFlavor = AppFlavor.PLAY,
         versionMax = 101,
         isVisible = true,
     ),
