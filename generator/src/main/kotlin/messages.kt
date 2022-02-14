12a1c83ef3d3c85b9637458c46d267404cbd938b
Mikołaj Pich <m.pich@outlook.com>
2022-02-14 09:59:49 +0100

    Hide resman.pl message

--- generator/src/main/kotlin/messages.kt
+++ generator/src/main/kotlin/messages.kt
@@ -9,7 +9,7 @@ val messages = listOf(
         targetRegisterHost = RegisterAddress.RESMAN.url,
         type = MessageType.DASHBOARD_MESSAGE,
         isDismissible = false,
-        isVisible = true,
+        isVisible = false,
     ),
     Message(
         id = 5,
