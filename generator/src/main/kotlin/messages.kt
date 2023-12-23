5cffe53b0ed7f70c7d7493094164f43ea4d62555
Mikołaj Pich <m.pich@outlook.com>
2023-12-23 11:12:53 +0100

    Remove versionMin from latest message

--- generator/src/main/kotlin/messages.kt
+++ generator/src/main/kotlin/messages.kt
@@ -12,7 +12,6 @@ val messages = listOf(
             MessageType.DASHBOARD_MESSAGE,
             MessageType.LOGIN_MESSAGE,
         ),
-        versionMin = 138,
         isDismissible = false,
         isVisible = true,
     ),
